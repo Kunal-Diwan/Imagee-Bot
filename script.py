4d0da729fa019f0f48f91569f49e2ad9dfb5ae4c
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-07-06 15:07:00 +0530

    Imagee #1

---
+++ script.py
@@ -0,0 +1,24 @@
+class script(object):
+
+    START_MSG = """ <b>Hi {}, I am Imagee . I am an Image editor bot . 
+
+I can edit Images with various modes .
+
+Check help for more ......\n\nPowered by @DevelopedBots 💖 </b>"""
+    HELP_MSG = """Hi, Follow this steps ..
+
+<code>🌀 Send me any Image to Edit..</code>
+
+<code>🌀 Select the corresponding mode that you need</code>
+
+<code>🌀 Your Edited Image will be Uploaded </code>
+
+
+Developed by @kunaldiwan"""
+    ABOUT_MSG = """<b>My name 🤖 : Imagee</b>
+
+<b>Language 🏳‍🌈 :</b> <code>Python3</code>
+
+<b>Library 📚 :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a>
+
+<b>Source code 💻 :</b> 👉 <a href='https://github.com/DevelopedBots/Imagee-Bot'>Here</a>"""
